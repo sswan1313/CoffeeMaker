f147a14e14977cc9130f0cef36f6575e5a95c011
sswan1313 <63887520+sswan1313@users.noreply.github.com>
2020-05-01 18:33:35 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+/*******************************************
+Coffee Maker - A coffee maker program
+Code by Samantha Swan
+********************************************/
+
+#include <stdio.h>
+#include "help.h"
+
+int main (itn argc, char *argv[]) {
+  //If no cmdline args, display help
+  if (i == argc) {
+  held_display();
+  return 42;
+  }
+
+  printf("Coffee Maker");
+  printf("Code by Samantha Swan");
+
+  return 43;
+}
